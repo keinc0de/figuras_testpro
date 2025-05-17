7c1ea4a6b99df0d63bab9f9112ae39b4e2e68668
kein <106981240+keinc0de@users.noreply.github.com>
2025-05-17 18:08:57 -0400

    create line horizontal function

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
-def test_uno() -> str:
-    return 'resultado uno (1)'
+from figures import Draw
 
-def escribe(text:str):
+
+def test() -> str:
+    draw = Draw()
+    text = 'TEST\n'
+    text += f'Linea horizontal:\n{draw.line.horizontal(8)}\n\n'
+    return text
+
+def escribeArchivo(text:str):
     with open ('salida_test.txt', 'w') as file:
         file.write(text)
 
+
 if __name__ == '__main__':
-    escribe(test_uno())
+    escribeArchivo(test())

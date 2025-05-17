dd78121fbcb57876af4b65283447b39dfd953b2f
kein <106981240+keinc0de@users.noreply.github.com>
2025-05-17 14:28:52 -0400

    workflow: create artifact from test file python

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
-def test_uno():
-    return 'resultado uno'
+def test_uno() -> str:
+    return 'resultado uno (1)'
+
+def escribe(text:str):
+    with open ('salida_test.txt', 'w') as file:
+        file.write(text)
 
 if __name__ == '__main__':
-    print(test_uno())
+    escribe(test_uno())
